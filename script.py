7ccff49e29a2a37d81208a4f92eabc6f8389d1a6
hassansultani757-png <hassn.sultani.757@gmail.com>
2026-07-25 23:16:44 +0430

    Add files via upload

---
+++ script.py
@@ -0,0 +1,38 @@
+"""
+x=int(input("enter first number: "))
+y=int(input("enter second number: "))
+print("x+y={}".format(x+y))
+print("x-y={}".format(x-y))
+print("x*y={}".format(x*y))
+print("x/y={}".format(x/y))
+print("x**y={}".format(x**y))
+print("x//y={}".format(x//y))
+"""
+"""
+print("mahdi"!="Mahdi")
+"""
+"""
+age=int(input("PLEASE ENTER YOUR AGE: "))
+if 13<age<=18:
+    print("teenager")
+elif age<=13:
+    print("child")  
+elif 30>age>18 :
+    print("young")
+elif 150>age>30:
+    print("old")
+else:
+    print("error")    
+ """
+ """
+name=input ("name: ")
+password =int(input("password: "))
+if name=="mahdi" and password ==5959:
+   print('ok')
+elif  name =="harry" and password ==189189:
+   print("ok")
+elif name=="esa" and password ==5959189189:
+   print("ok") 
+else:
+   print("error") 
+ """   
