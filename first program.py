96d7117cffebf10a65d3f115c42522fa3a590405
hassansultani757-png <hassn.sultani.757@gmail.com>
2026-07-25 23:10:39 +0430

    Add files via upload

---
+++ first program.py
@@ -0,0 +1,6 @@
+# اینجا ما اسم کاربر را میگیریم 
+name =input ("please enter your name: ")
+# اینجا ما اسم کاربر را نمایش می‌دهیم
+lastname=input ('please enter your last name: ')
+print("your full name is: ",name, lastname)
+#print(name, lastname)
