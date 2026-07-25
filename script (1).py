752e0c7f00d7e301b833db81f5d352f72d90af1a
hassansultani757-png <hassn.sultani.757@gmail.com>
2026-07-25 23:21:43 +0430

    Add files via upload

---
+++ script (1).py
@@ -0,0 +1,6 @@
+def welcome(name,age,city="kabul"):
+ print("welcome",name )
+ print ("age:",age)
+ print("city:",city)
+ print("your full details:you live in",city,"your age is:",age,"your name is:",name)
+welcome("zaymor",17,)
