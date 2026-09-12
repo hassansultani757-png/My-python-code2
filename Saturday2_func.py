3f8023c171e3eafdde10427264de8e4ed3dcda18
hassansultani757-png <hassn.sultani.757@gmail.com>
2026-09-12 21:41:20 +0430

    Add files via upload

---
+++ Saturday2_func.py
@@ -0,0 +1,14 @@
+while True:
+    def number(num):
+    
+        return num1%2
+    num1=float (input("number: "))
+    if num1 ==0:
+        print ("None")
+        continue  
+    a=number(num1)
+    if a ==0:
+        print("True")
+    else :
+        print ("False")    
+   
