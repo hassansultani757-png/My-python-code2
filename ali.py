89eb841e0b1433e51648cf58a0749dcce11f47ca
hassansultani757-png <hassn.sultani.757@gmail.com>
2026-09-19 22:04:14 +0430

    Add files via upload

---
+++ ali.py
@@ -0,0 +1,14 @@
+s=0
+b=0
+while True :
+    def average (za):
+        return za
+    a=int (input("num : "))
+    if a==0:
+        break 
+    s+=a
+    b+=1
+    av=s/b
+    m= average (av) 
+print (m)     
+    
